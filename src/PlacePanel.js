ffb49f9e3d6f779432ddf03d715b35a95a6e0962
fpassow <fpassow@yahoo.com>
2019-01-23 15:43:13 -0500

    Bought a ship

--- src/PlacePanel.js
+++ src/PlacePanel.js
@@ -2,20 +2,26 @@ import React from 'react'
 import { connect } from 'react-redux'
 import ShipsForSale from './ShipsForSale'
 import { currentPlaceInfo } from './stateUtils'
+import { buyShip } from './actions'
 
-const PlacePanelComponent = ({place}) => (
+const PlacePanelComponent = ({place, cash, buyShip}) => (
     <div >
         <h2>{place.name}</h2>
         <h3>{place.description}</h3>
-        <ShipsForSale shipsForSale={place.shipsForSale} />
+        <ShipsForSale shipsForSale={place.shipsForSale} cash={cash} buyShip={buyShip} />
     </div>
 )
 
-
 const mapStateToProps = state => ({
-  place: currentPlaceInfo(state)
+  place: currentPlaceInfo(state),
+  cash: state.cash
+})
+
+const mapDispatchToProps = dispatch => ({
+  buyShip: (shipId) => { dispatch(buyShip(shipId)) }
 })
 
 export default connect(
-  mapStateToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(PlacePanelComponent)
--- src/ShipsForSale.js
+++ src/ShipsForSale.js
@@ -1,18 +1,21 @@
 import React from 'react'
 
-export default function ShipsForSaleComponent({shipsForSale}) {
+export default function ShipsForSaleComponent({shipsForSale, cash, buyShip}) {
     return (
         <div >
             <h3>Ships for Sale</h3>
-            {shipsForSale.map((ship)=><ShipForSale key={ship.shipId} ship={ship} />)}
+            {shipsForSale.map((ship)=><ShipForSale key={ship.shipId} buyShip={buyShip} ship={ship} canBuy={ship.basePrice <= cash}/>)}
         </div>
     )
 }
 
-function  ShipForSale({ship}) {
+function  ShipForSale({ship, canBuy, buyShip}) {
     return (
         <div >
-            {ship.shipTypeName} <em>{ship.shipName}</em> {ship.basePrice}&#402;
+            {ship.shipTypeName} &nbsp;
+            <em>{ship.shipName}</em> &nbsp;
+            {ship.basePrice}&#402; &nbsp;
+            {canBuy ? <button onClick={()=>{buyShip(ship.shipId)}}>buy</button> : ' '}
         </div>
     )
 }
