522a7848602193ecf72a24a547310fe56da5bdd1
fpassow <fpassow@yahoo.com>
2019-02-11 14:59:43 -0500

    My ship appears on a map square

--- src/MainPage.js
+++ src/MainPage.js
@@ -4,9 +4,9 @@ import ShipPanel from './ship/ShipPanel'
 import ShiplessPanel from './ship/ShiplessPanel'
 import MapPanel from './MapPanel'
 
-const MainPage = ({showWelcomePage, hasShip}) => (
+const MainPage = ({showWelcomePage, hasShip, isInPort}) => (
     <div >
-        <PlacePanel />
+        {isInPort ? <PlacePanel /> : <div>At sea....</div>}
         {hasShip ? <ShipPanel /> : <ShiplessPanel />}
         <MapPanel />
         <button onClick={showWelcomePage} >Help</button>
--- src/MapPanel.js
+++ src/MapPanel.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import TimeView from './TimeView'
+import * as stateUtils from './stateUtils'
 
 const mapStyle = {
     display: 'grid',
-    gridTemplateColumns: 'repeat(6, 100px)',
-    gridTemplateRows: 'repeat(6, 100px)'
+    gridTemplateColumns: 'repeat(6, 200px)',
+    gridTemplateRows: 'repeat(6, 200px)'
 }
 
-const MapPanelComponent = ({ticks, days, ticksToday, places}) => (
-    <div >
+const Square = ({p, myShip}) => (
+    <div style={{gridColumn: p.x, gridRow: p.y}} key={p.placeId}>
+        {p.placeId} {(myShip && (myShip.placeId === p.placeId)) ? <span>{myShip.shipName}</span> : <span> </span>} 
+    </div>
+)
+
+const MapPanelComponent = ({ticks, days, ticksToday, places, myShip}) => (
+    <div>
         <TimeView days={days} ticks={ticks} ticksToday={ticksToday} />
         <div style={mapStyle}>
-            {places.map((p)=>(<div style={{gridColumn: p.x, gridRow: p.y}} key={p.placeId}>{p.placeId}</div>))}
+            {places.map((p)=>(<Square key={p.placeId} p={p} myShip={myShip} />))}
         </div>
     </div>
 )
@@ -22,7 +29,8 @@ const mapStateToProps = state => ({
   days: state.days,
   ticks: state.ticks,
   ticksToday: state.ticksToday,
-  places: state.places
+  places: state.places,
+  myShip: stateUtils.getMyShip(state)
 })
 
 export default connect(
