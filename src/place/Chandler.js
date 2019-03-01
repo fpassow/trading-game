be7ecf9475d85661ea0e7e007a975d940bd12c69
fpassow <fpassow@yahoo.com>
2019-03-01 17:30:31 -0500

    Added images for rations and olive oil. Added module for mapping cargo types to image file names

--- src/place/Chandler.js
+++ src/place/Chandler.js
@@ -3,8 +3,7 @@ import React from 'react'
 const Chandler = ({place, buyFood, hasShip}) => {
     return (
         <div>
-            <h3>Chandler</h3>
-            Rations: {place.foodPrice}  &#402; {hasShip ? <button onClick={buyFood}>Buy</button> : ''}
+            Rations <img alt="" src="rations.jpg"/> {place.foodPrice}  &#402; {hasShip ? <button onClick={buyFood}>Buy</button> : ''}
         </div>
     )
 }
