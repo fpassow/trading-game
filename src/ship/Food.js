18832ab70808d9647405ae01f66b19f1e8a27401
fpassow <fpassow@yahoo.com>
2019-03-11 16:25:06 -0400

    Added arrows to show where you can move. Still need to set z axis and center them.

--- src/ship/Food.js
+++ src/ship/Food.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Food = ({myShip}) => {
     let foodArr = []
     for (let i = 0; i < myShip.food; i++) {
-        foodArr.push(<img src="rations.jpg" alt="rations" />)
+        foodArr.push(<img src="rations.jpg" alt="rations" key={i} />)
     }
     return (
         <div>
