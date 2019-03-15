27ae6ba1804c5c3bda10d51c03eeabc88cefcf65
fpassow <fpassow@yahoo.com>
2019-03-15 16:51:08 -0400

    Add button to start a new game after you starve

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from 'react'
 import { connect } from 'react-redux'
 import WelcomePage from './WelcomePage'
 import MainPage from './MainPage'
-import { showWelcomePage } from './actions'
+import { showWelcomePage, startNewGame } from './actions'
 import * as stateUtils from './stateUtils'
 
-const AppComponent = ({showingWelcomePage, showWelcomePage, hideWelcomePage, hasShip, isInPort, gameOver, gameOverMessage}) => {
+const AppComponent = ({showingWelcomePage, showWelcomePage, hideWelcomePage, hasShip, isInPort, gameOver, gameOverMessage, startNewGame}) => {
     if (gameOver) {
-        return <div className="App"><br /><br /><br /><h1>Game Over: {gameOverMessage}</h1></div>
+        return (
+            <div className="App" style={{textAlign:'center'}}>
+                <br /><br /><br />
+                <h1>Game Over: {gameOverMessage}</h1>
+                <button onClick={startNewGame}>Start a new game</button>
+            </div>
+        )
     } else if (showingWelcomePage) {
         return <div className="App"><WelcomePage hideWelcomePage={hideWelcomePage} /></div>
     } else {
@@ -25,7 +31,8 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
   showWelcomePage: () => dispatch(showWelcomePage(true)),
-  hideWelcomePage: () => dispatch(showWelcomePage(false))
+  hideWelcomePage: () => dispatch(showWelcomePage(false)),
+  startNewGame: () => dispatch(startNewGame())
 })
 
 export default connect(
