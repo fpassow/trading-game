511f53db46a33686259d7597f772f81ebbb5e6dd
fpassow <fpassow@yahoo.com>
2019-05-18 13:32:59 -0400

    Rations display finished using barrels and empty spots

--- src/ship/Food.js
+++ src/ship/Food.js
@@ -5,11 +5,13 @@ const Food = ({myShip}) => {
     for (let i = 0; i < myShip.food; i++) {
         foodArr.push(<img src="img/rations.png" alt="rations" key={i} />)
     }
+    for (let i = 0; i < (myShip.maxFood - myShip.food); i++) {
+        foodArr.push(<img src="img/no_rations.png" alt="Empty rations bay" key={'e'+i} />)
+    }
     let foodStyle = {
         border: '2px solid black',
         padding: '5px',
-        margin: '5px',
-        height: '40px'
+        margin: '5px'
     }
     return (
         <div style={foodStyle}>
