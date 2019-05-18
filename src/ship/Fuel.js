b7e3f378f0793498e66a726a92e086a40a9b69b1
fpassow <fpassow@yahoo.com>
2019-05-18 14:05:07 -0400

    Images and code for fuel display.

--- src/ship/Fuel.js
+++ src/ship/Fuel.js
@@ -1,12 +1,22 @@
 import React from 'react'
 
-const Fuel = ({myShip}) => {
+const Fuel = ({myShip}) =>  {
+    let fuelArr = []
+    for (let i = 0; i < myShip.fuel; i++) {
+        fuelArr.push(<img src="img/fuel.png" alt="Fuel barrel" key={i} />)
+    }
+    for (let i = 0; i < (myShip.maxFuel - myShip.fuel); i++) {
+        fuelArr.push(<img src="img/no_fuel.png" alt="Empty fuel bay" key={'e'+i} />)
+    }
+    let fuelStyle = {
+        border: '2px solid black',
+        padding: '5px',
+        margin: '5px'
+    }
     return (
-        <div>
-            <h3>Fuel</h3>
-            {myShip.fuel} tonnes (Capacity: {myShip.maxFuel} tonnes)
+        <div style={fuelStyle}>
+            {fuelArr}
         </div>
     )
 }
-
 export default Fuel
